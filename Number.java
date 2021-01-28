a62ffc9872f3ed47e1c61f3800231d4a90547152
martuaanggasana@gmail.com <martuaanggasana@gmail.com>
2021-01-28 14:49:31 +0700

    Number-ByArya

---
+++ Number.java
@@ -0,0 +1,8 @@
+public class Number {
+    public static void main(String args[]) {
+      System.out.println("Helo Dunia Seputar Kehidupan");
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
